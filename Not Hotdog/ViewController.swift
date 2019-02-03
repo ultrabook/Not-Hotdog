d1a1c24f6101c43c3927608167ba82f5adea411f
Randy Hsu <randyhsu2010@gmail.com>
2019-02-02 20:05:58 -0800

    Initial Commit

---
+++ Not Hotdog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Not Hotdog
+//
+//  Created by Randy Hsu on 2019-02-02.
+//  Copyright © 2019 DeveloperRandy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
